5cd525aca63a7b1bf09e0e679174a10f176ccc98
mo khan <mo.khan@gmail.com>
2019-10-19 16:03:27 -0600

    Accept a parameter

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello()
-  want := "Hello, world"
+  got := Hello("mo")
+  want := "Hello, mo"
 
   if got != want {
     t.Errorf("got %q want %q", got, want)
