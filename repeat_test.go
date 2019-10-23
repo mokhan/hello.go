55b4c85011aa17373000dbc5a64ec7bf48ccc4aa
mo khan <mo.khan@gmail.com>
2019-10-22 20:11:39 -0600

    start test to specify times

--- repeat_test.go
+++ repeat_test.go
@@ -3,12 +3,23 @@ package main
 import "testing"
 
 func TestRepeat(t *testing.T) {
-  repeated := Repeat("a")
-  expected := "aaaaa"
+  t.Run("default", func(t *testing.T) {
+    repeated := Repeat("a")
+    expected := "aaaaa"
 
-  if repeated != expected {
-    t.Errorf("expected %q but got %q", expected, repeated)
-  }
+    if repeated != expected {
+      t.Errorf("expected %q but got %q", expected, repeated)
+    }
+  })
+
+  t.Run("custom interval", func(t *testing.T) {
+    repeated := Repeat("a", 10)
+    expected := "aaaaaaaaaa"
+
+    if repeated != expected {
+      t.Errorf("expected %q but got %q", expected, repeated)
+    }
+  })
 }
 
 func BenchmarkRepeat(b *testing.B) {
